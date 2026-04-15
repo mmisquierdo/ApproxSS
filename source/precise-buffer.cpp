df652599ae3f7ceed3cbb39d048e8e122a5043e7
mmisquierdo <mmisquierdo@inf.ufpel.edu.br>
2026-04-15 15:48:57 -0700

    precise-buffer

---
+++ source/precise-buffer.cpp
@@ -0,0 +1,48 @@
+#include "precise-buffer.h"
+
+PreciseBuffer::~PreciseBuffer() {
+    TrackingBuffer::~TrackingBuffer();
+}
+
+PreciseBuffer::PreciseBuffer(const Range& bufferRange, const int64_t id, const uint64_t creationPeriod, const size_t dataSizeInBytes, const InjectionConfigurationReference& injectorCfg) : 
+	TrackingBuffer<true>(bufferRange, id, creationPeriod, dataSizeInBytes, injectorCfg.GetBitDepth(), injectorCfg.GetConfigurationId()),	
+    m_configurationId(injectorCfg.GetConfigurationId()),
+    m_bitDepth(injectorCfg.GetBitDepth()) {}
+
+int64_t PreciseBuffer::GetConfigurationId() const {
+    return this->m_configurationId;
+}
+
+size_t PreciseBuffer::GetBitDepth() const {
+    return this->m_bitDepth;
+}
+
+const InjectionConfigurationReference& PreciseBuffer::GetInjectionConfigurationReference() const {
+    const auto it = g_injectorConfigurations.find(this->GetConfigurationId()); //WARNING: this also may crash the tool, but it should have crashed before
+
+	return *(it->second.get());
+}
+
+void PreciseBuffer::HandleMemoryWriteSIMD(uint8_t * const initialAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread)) {
+	this->m_periodLog.IncreaseAccess(false IF_COMMA_PIN_LOCKED(isBufferInThread), AccessTypes::Write, accessSize);
+}
+
+void PreciseBuffer::HandleMemoryWriteSingleElementSafe(uint8_t * const accessedAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread)) {
+    this->m_periodLog.IncreaseAccess(false IF_COMMA_PIN_LOCKED(isBufferInThread), AccessTypes::Write, accessSize);
+}
+
+void PreciseBuffer::HandleMemoryWriteScattered(IMULTI_ELEMENT_OPERAND const * const memOpInfo, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread)) {
+	this->m_periodLog.IncreaseAccess(false IF_COMMA_PIN_LOCKED(isBufferInThread), AccessTypes::Write, this->m_dataSizeInBytes * memOpInfo->NumOfElements());
+}
+
+void PreciseBuffer::HandleMemoryReadSIMD(uint8_t * const initialAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread)) {
+	this->m_periodLog.IncreaseAccess(false IF_COMMA_PIN_LOCKED(isBufferInThread), AccessTypes::Read, accessSize);
+}
+
+void PreciseBuffer::HandleMemoryReadSingleElementSafe(uint8_t * const accessedAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread)) {
+    this->m_periodLog.IncreaseAccess(false IF_COMMA_PIN_LOCKED(isBufferInThread), AccessTypes::Read, accessSize);
+}
+
+void PreciseBuffer::HandleMemoryReadScattered(IMULTI_ELEMENT_OPERAND const * const memOpInfo, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread)) {
+	this->m_periodLog.IncreaseAccess(false IF_COMMA_PIN_LOCKED(isBufferInThread), AccessTypes::Read, this->m_dataSizeInBytes * memOpInfo->NumOfElements());
+}
---
+++ source/precise-buffer.h
@@ -0,0 +1,40 @@
+#ifndef PRECISE_BUFFER_H
+#define PRECISE_BUFFER_H
+
+#include "range.h"
+#include "period-log.h"
+#include "injector-configuration.h"
+#include "tracking-buffer.h"
+
+class PreciseBuffer : public TrackingBuffer<true> {
+	protected:
+		const int64_t m_configurationId;
+		const size_t m_bitDepth;
+
+	public:
+		PreciseBuffer(const Range& bufferRange, const int64_t id, const uint64_t creationPeriod, const size_t dataSizeInBytes,
+					  const InjectionConfigurationReference& injectorCfg);
+
+		PreciseBuffer(const PreciseBuffer&) = delete;
+		PreciseBuffer(const PreciseBuffer&&) = delete;
+
+		~PreciseBuffer();
+		
+		virtual int64_t GetConfigurationId() const;
+        virtual size_t GetBitDepth() const;
+
+		virtual const InjectionConfigurationReference& GetInjectionConfigurationReference() const;
+
+		virtual void HandleMemoryReadSIMD(uint8_t * const initialAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread));
+		virtual void HandleMemoryWriteSIMD(uint8_t * const initialAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread));
+		virtual void HandleMemoryReadSingleElementSafe(uint8_t * const accessedAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread));
+		virtual void HandleMemoryWriteSingleElementSafe(uint8_t * const accessedAddress, const uint32_t accessSize, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread));
+		virtual void HandleMemoryReadScattered(IMULTI_ELEMENT_OPERAND const * const memOpInfo, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread));
+		virtual void HandleMemoryWriteScattered(IMULTI_ELEMENT_OPERAND const * const memOpInfo, const bool isThreadInjectionEnabled IF_COMMA_PIN_LOCKED(const bool isBufferInThread));	
+
+		//virtual void NextPeriod(const int64_t period);
+		//virtual void ReactivateBuffer(const int64_t creationPeriod);
+		//virtual bool RetireBuffer(const bool giveAwayRecords);
+};
+
+#endif /* PRECISE_BUFFER_H */
